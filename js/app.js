a52e2c79faef885c5f6068bf951c6c021f2e6190
David-Masabanda <josue.masabanda99@gmail.com>
2023-06-07 19:42:30 -0500

    Taller13

---
+++ js/app.js
@@ -0,0 +1,18 @@
+console.log("Componente Vue")
+console.log(Vue)
+
+const app=Vue.createApp({
+    //Opcion 1
+    template:`
+    <h1>Hola mundo</h1>
+    <p>Desde Vue js</p>
+    `
+    
+    //Opcion 1
+
+    //Opcion n ......
+
+
+})
+
+app.mount('#myAplicacion')
