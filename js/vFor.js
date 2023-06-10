2b202147d767e13563d7c0b1b0bafb07cfa166b5
David-Masabanda <josue.masabanda99@gmail.com>
2023-06-10 10:47:49 -0500

    Taller15

--- js/vFor.js
+++ js/vFor.js
@@ -1,8 +1,68 @@
-//Options API
-const app=Vue.createApp({
+var estudiantes = [
+    { nombre: "David", apellido: "Masabanda" },
+    { nombre: "Valeria", apellido: "Ordoñez" },
+    { nombre: "Samara", apellido: "Vizcaino" },
+    { nombre: "Sebitas", apellido: "Arguello" },
+    { nombre: "Jaime", apellido: "Jimenez" },
+    { nombre: "Fernanda", apellido: "Mosquera" },
+    { nombre: "Axel", apellido: "Lopez" },
+    { nombre: "David", apellido: "Obando" },
+    { nombre: "Lucia", apellido: "Paez" },
+    { nombre: "Sara", apellido: "Martinez" }
+];
 
+
+
+console.log(estudiantes)
+
+//Options API
+const app = Vue.createApp({
+    data() {
+        return {
+            estudiantes,
+            nombre:'Samara'
+        }
+    },
+
+    methods:{
+        agregarEstudiante(event){
+            console.log("Vamos a agregar")
+            console.log(this.nombre)
+            console.log(event)
+            console.log(event.charCode)
+            if (event.charCode==13) {
+                console.log("Presiono enter")
+                const nuevoEstudiante={
+                    nombre:this.nombre,
+                    apellido:"Marks",
+                }
+                //Agrega al inicio
+                //this.estudiantes.unshift(nuevoEstudiante)
+                //Agrega al final
+                this.estudiantes.push(nuevoEstudiante)
+            }else{
+                console.log("No presiono enter")
+            }
+            console.log(estudiantes)
+            //estudiantes.push(this.nombre)
+        },
+
+        agregarEstudiante2(){
+            console.log("Vamos a agregar")
+            console.log(this.nombre)
+            const nuevoEstudiante={
+                nombre:this.nombre,
+                apellido:"Marks",
+            }
+            //Agrega al inicio
+            //this.estudiantes.unshift(nuevoEstudiante)
+            //Agrega al final
+            this.estudiantes.push(nuevoEstudiante)
+            console.log(estudiantes)
+        }
+    }
 
 
 })
 
-app.mount('#myAplicacion')
+app.mount('#myAplicacion2')
